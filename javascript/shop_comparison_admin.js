0f1f63264579b0f8c6b5a260e5d0defc1f880558
Will Rossiter <will@fullscreen.io>
2026-05-11 15:27:26 +1200

    feat: remove jquery requirement

--- javascript/shop_comparison_admin.js
+++ javascript/shop_comparison_admin.js
@@ -1,16 +1,52 @@
-(function($) {
-	$("body").on('change', '.silvershop-on_feature_select_fetch_value_field', function(e) {
-		var value = $(this).parents("td").siblings(".col-Value"),
-			data = {
-				ID: $(this).val(),
-				SecurityID: $("input[name=SecurityID]").val(),
-				Name: value.find(':input').first().attr('name')
-			};
+(function () {
+    'use strict';
 
-		value.html('');
+    document.body.addEventListener('change', function (e) {
+        var t = e.target;
+        if (!(t instanceof Element)) {
+            return;
+        }
 
-		$.get('ProductFeatureValueFieldController', data, function(data) {
-			value.html(data);
-		});
-	});
-})(jQuery);
+        if (!t.matches('.silvershop-on_feature_select_fetch_value_field')) {
+            return;
+        }
+
+        var row = t.closest('tr');
+        if (!row) {
+            return;
+        }
+
+        var valueCell = row.querySelector('.col-Value');
+        if (!valueCell) {
+            return;
+        }
+
+        var sec = document.querySelector('input[name=SecurityID]');
+        var securityId = sec instanceof HTMLInputElement ? sec.value : '';
+
+        var inputInValue = valueCell.querySelector('input, select, textarea');
+        var nameAttr = inputInValue instanceof HTMLElement ? inputInValue.getAttribute('name') : '';
+
+        var params = new URLSearchParams({
+            ID: String(t.value || ''),
+            SecurityID: securityId,
+            Name: nameAttr || ''
+        });
+
+        valueCell.innerHTML = '';
+
+        fetch('ProductFeatureValueFieldController?' + params.toString(), {
+            credentials: 'same-origin',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (html) {
+                valueCell.innerHTML = html;
+            })
+            .catch(function () {});
+    });
+})();
